9c527182aa23f71e5a52d843f5766a6267d217e6
Manuel Huber <manuel.h87@gmail.com>
2022-01-16 10:33:35 +0100

    Enhance info command (add --debug flag)

--- src/imagesort/commands/info.py
+++ src/imagesort/commands/info.py
@@ -3,18 +3,19 @@ from ..exif import ExifTagsExtractor, UnsupportedImageFormat
 
 
 @click.command(name="info")
+@click.option("--debug", is_flag=True)
 @click.argument("path", type=click.Path(
     exists=True, dir_okay=False, file_okay=True, allow_dash=False,
     path_type=str
 ))
-def main(path):
+def main(path, debug):
     try:
-        _main(path)
+        _main(path, debug)
     except UnsupportedImageFormat as e:
         print(f"File {e.path} can't be load as image")
 
 
-def _main(path):
-    exif = ExifTagsExtractor(path)
+def _main(path, debug):
+    exif = ExifTagsExtractor(path, verbose=debug)
     print(f"File {path}")
     print(f"  - Creation Date: {exif.get_creation_date()}")
