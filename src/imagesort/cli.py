ab3bf4ef9014be9ff511cbb5fed39164b4ed54b4
Manuel Huber <manuel.h87@gmail.com>
2022-01-16 10:40:02 +0100

    Add --quiet flag

--- src/imagesort/cli.py
+++ src/imagesort/cli.py
@@ -6,7 +6,7 @@ from .commands import sort, info
 _LOG_FORMAT = "{name:<15} : {threadName:>15} : {levelname:>8} : {message}"
 _DEFAULT_LOG_LEVEL = logging.INFO
 
-def _init_logging(verbose):
+def _init_logging(verbose, quiet):
     handler = logging.StreamHandler(sys.stderr)
     logging.basicConfig(format=_LOG_FORMAT,
                         style='{',
@@ -15,13 +15,16 @@ def _init_logging(verbose):
     level = _DEFAULT_LOG_LEVEL
     if verbose:
         level = logging.DEBUG
+    elif quiet:
+        level = logging.WARNING
     handler.setLevel(level)
 
 
 @click.group()
 @click.option('-v', '--verbose', is_flag=True)
-def main(verbose):
-    _init_logging(verbose)
+@click.option('-q', '--quiet', is_flag=True)
+def main(verbose, quiet):
+    _init_logging(verbose, quiet)
 
 
 main.add_command(sort.main)
